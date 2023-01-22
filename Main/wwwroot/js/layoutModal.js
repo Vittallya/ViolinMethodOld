17f53f0cdcdd56b98cfdfab63e2a434943e10d8c
VIT <Vit.Por@bk.ru>
2023-01-22 17:16:34 +0300

    Еще всякие мелкие правки

--- Main/wwwroot/js/layoutModal.js
+++ Main/wwwroot/js/layoutModal.js
@@ -1,7 +1,8 @@
 ﻿const MODAL_ID = "layout_modal"
 const MODAL_BODY_ID = "modal_root"
 const MODAL_HEADER_ID = "layout_modal_header"
-
+const MODAL_OK_BTN_ID = "bt_modal_ok"
+const MODAL_FOOTER_ID = "layout_modal_footer"
 
 function modalShowErrors(errors, text = 'Ошибка') {
     $("#"+MODAL_ID).modal('show')
@@ -10,6 +11,8 @@ function modalShowErrors(errors, text = 'Ошибка') {
     errors.forEach(error => {
         ul.append(`<li>${error}</li>`)
     })
+
+    $("#" + MODAL_FOOTER_ID).removeClass("d-none")
     $("#" + MODAL_BODY_ID).empty().append(ul)
 }
 
@@ -22,6 +25,10 @@ function modalGetSpinner() {
 function modalSetupSpinner() {
     $("#" + MODAL_ID).modal('show')
     $("#" + MODAL_HEADER_ID).text("")
+
+    if (!$("#" + MODAL_FOOTER_ID).hasClass("d-none"))
+        $("#" + MODAL_FOOTER_ID).addClass("d-none")
+
     $("#" + MODAL_BODY_ID).empty().append(modalGetSpinner())
 }
 
